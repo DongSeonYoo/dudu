a23f902cbe874975a2736f08b784e6d842835c01
ehdtjs0612 <inko51366@naver.com>
2024-08-12 15:21:55 +0900

    feat: add formatTimeToHHMM in attendanceUtil

--- src/utils/attendance.util.ts
+++ src/utils/attendance.util.ts
@@ -15,4 +15,10 @@ export class DateUtilService {
 
     return tomorrow;
   }
+
+  formatTimeToHHMM(date: Date = new Date()): string {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
 }

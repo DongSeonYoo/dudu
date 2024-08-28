daac362d45b726045917ba5cd40525987449a10e
ehdtjs0612 <inko51366@naver.com>
2024-08-28 22:47:31 +0900

    feat: add date util functions (addDays)

--- src/utils/date-util/date-util.service.ts
+++ src/utils/date-util/date-util.service.ts
@@ -42,4 +42,11 @@ export class DateUtilService {
   stringToUtcDate(dateString: string): Date {
     return dayjs(dateString).toDate();
   }
+
+  /**
+   * 받은 날짜에 특정 기간 (일)을 더합니다.
+   */
+  addDays(date: Date, days: number): Date {
+    return dayjs(date).add(days, 'day').toDate();
+  }
 }

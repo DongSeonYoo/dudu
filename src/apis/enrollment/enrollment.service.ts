40fe5719f0c3bf4cd6037ce7e3299654f6fa7d2b
ehdtjs0612 <inko51366@naver.com>
2024-09-02 20:12:10 +0900

    refactor: modify createEnrollmentInfo to return data object instead of Entity

--- src/apis/enrollment/enrollment.service.ts
+++ src/apis/enrollment/enrollment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { EnrollmentEntity } from './entity/enrollment.entity';
+import { EnrollmentEntity, IEnrollment } from './entity/enrollment.entity';
 import { $Enums } from '@prisma/client';
 import { DateUtilService } from 'src/utils/date-util/date-util.service';
 
@@ -10,20 +10,22 @@ export class EnrollmentService {
   private readonly DEFAULT_ENROLLMENT_PERIOD = 30;
   constructor(private readonly dateUtilService: DateUtilService) {}
 
-  createEnrollmentInfo(type: $Enums.TYPE, month: number): EnrollmentEntity {
-    const amount = this.calculateAmount(type);
-    const startedAt = new Date();
-    return EnrollmentEntity.create({
-      amount: amount,
+  createEnrollmentInfo(
+    type: $Enums.TYPE,
+    startedAt: Date,
+    month: number,
+  ): IEnrollment.ICreateEnrollment {
+    return {
+      amount: this.calculateAmount(type),
       startedAt: startedAt,
       endedAt: this.dateUtilService.addDays(
         startedAt,
         this.DEFAULT_ENROLLMENT_PERIOD * month,
       ),
-    });
+    };
   }
 
-  calculateAmount(type: $Enums.TYPE): number {
+  private calculateAmount(type: $Enums.TYPE): number {
     return type === 'REAPEATER' ? this.REAPEATER_AMOUNT : this.STUDENT_AMOUNT;
   }
 }

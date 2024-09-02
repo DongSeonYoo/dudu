91346de801cfb613a3ae98f35ffa328bd274273f
ehdtjs0612 <inko51366@naver.com>
2024-09-02 21:19:52 +0900

    refactor: add getter/setter for encapsulation property

--- src/apis/enrollment/enrollment.service.ts
+++ src/apis/enrollment/enrollment.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { EnrollmentEntity, IEnrollment } from './entity/enrollment.entity';
+import { IEnrollment } from './entity/enrollment.entity';
 import { $Enums } from '@prisma/client';
 import { DateUtilService } from 'src/utils/date-util/date-util.service';
 
 @Injectable()
 export class EnrollmentService {
-  private readonly STUDENT_AMOUNT = 650000;
-  private readonly REAPEATER_AMOUNT = 500000;
-  private readonly DEFAULT_ENROLLMENT_PERIOD = 30;
+  private readonly _STUDENT_AMOUNT = 650000;
+  private readonly _REAPEATER_AMOUNT = 500000;
+  private readonly _DEFAULT_ENROLLMENT_PERIOD = 30;
   constructor(private readonly dateUtilService: DateUtilService) {}
 
   createEnrollmentInfo(
@@ -20,12 +20,20 @@ export class EnrollmentService {
       startedAt: startedAt,
       endedAt: this.dateUtilService.addDays(
         startedAt,
-        this.DEFAULT_ENROLLMENT_PERIOD * month,
+        this._DEFAULT_ENROLLMENT_PERIOD * month,
       ),
     };
   }
 
   private calculateAmount(type: $Enums.TYPE): number {
-    return type === 'REAPEATER' ? this.REAPEATER_AMOUNT : this.STUDENT_AMOUNT;
+    return type === 'REAPEATER' ? this._REAPEATER_AMOUNT : this._STUDENT_AMOUNT;
+  }
+
+  get STUDENT_AMOUNT(): number {
+    return this._STUDENT_AMOUNT;
+  }
+
+  get REAPEATER_AMOUNT(): number {
+    return this._REAPEATER_AMOUNT;
   }
 }

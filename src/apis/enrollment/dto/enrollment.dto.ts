55c9997b9fc2548a393af79c8a1110c250ad7454
ehdtjs0612 <inko51366@naver.com>
2024-09-03 17:00:07 +0900

    refactor: improve validation to EnrollmentCreateRequestDto.month

--- src/apis/enrollment/dto/enrollment.dto.ts
+++ src/apis/enrollment/dto/enrollment.dto.ts
@@ -1,9 +1,7 @@
 import { PickType } from '@nestjs/swagger';
 import { EnrollmentEntity, IEnrollment } from '../entity/enrollment.entity';
-import { IsDate, IsNotEmpty, IsOptional, Validate } from 'class-validator';
+import { IsDate, IsNotEmpty, IsOptional, Max, Min } from 'class-validator';
 import { Type } from 'class-transformer';
-import { StartedAtEndedAtValidation } from 'src/validation/started-ended.validation';
-import { $Enums } from '@prisma/client';
 
 export class EnrollmentCreateRequestDto extends PickType(EnrollmentEntity, [
   'startedAt',
@@ -11,19 +9,17 @@ export class EnrollmentCreateRequestDto extends PickType(EnrollmentEntity, [
   @IsNotEmpty()
   @IsDate()
   @Type(() => Date)
-  @Validate(StartedAtEndedAtValidation)
   startedAt: Date;
 
   /**
    * 등록할 기간 (달)
    *
-   * month 사실 필요없을듯? startedAt, endedAt으로 계산 가능할듯
-   * dateUtilService클래스에서 커스텀 함수로 만들어서 사용하면 될뜼?
-   *
    * @example 1
    * @default 1
    */
   @IsOptional()
+  @Min(1)
+  @Max(12)
   month: number = 1;
 
   toEntity(args: IEnrollment.ICreateEnrollment): EnrollmentEntity {

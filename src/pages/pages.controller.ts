c56b5fc730512185ca5e40b16f317c537aa3af62
ehdtjs0612 <inko51366@naver.com>
2024-09-24 21:42:00 +0900

    feat: add comments to page routing controllers

--- src/pages/pages.controller.ts
+++ src/pages/pages.controller.ts
@@ -2,15 +2,25 @@ import fs from 'fs';
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import path, { join } from 'path';
+import { ApiTags } from '@nestjs/swagger';
+import { LoginAuthGuard } from 'src/decorators/jwt-auth.decorator';
 
+@ApiTags('Pages')
 @Controller()
 export class PagesController {
+  /**
+   * 루트 페이지 서빙
+   */
   @Get('/')
   serveIndexPage(@Res() res: Response) {
     return res.sendFile(path.join(__dirname, '..', '..', 'client'));
   }
 
+  /**
+   * 페이지 이름을 받아서 해당 페이지 서빙
+   */
   @Get(':page')
+  @LoginAuthGuard()
   servePage(@Param('page') page: string, @Res() res: Response) {
     return this.serveHtmlPage(page, res);
   }
